815a88cd4d42f42387a80ef8675dd434d86c90fe
wangyijia <wyj11281212@163.com>
2024-10-13 01:49:53 +0800

    Feat: 实现了文件系统接口

--- os/src/fs/mod.rs
+++ os/src/fs/mod.rs
@@ -48,5 +48,5 @@ bitflags! {
     }
 }
 
-pub use inode::{list_apps, open_file, OSInode, OpenFlags};
+pub use inode::{link_at, list_apps, open_file, unlink_at, OSInode, OpenFlags};
 pub use stdio::{Stdin, Stdout};
